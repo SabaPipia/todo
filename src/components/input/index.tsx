3099713047dfcad955ed91e2a6f738732b1e3b17
Ch34ry <pipiasaba788@gmail.com>
2023-06-17 20:24:52 +0400

    save

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Arrow } from "../../icons";
 import "./style.scss";
 
-const Input = () => {
+interface InputI {
+  addTodo: (text?: string, id?: number) => void;
+}
+
+const Input: React.FC<InputI> = ({ addTodo }) => {
+  const [value, setValue] = useState<string | undefined>();
+
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleSave = () => {
+    const input = inputRef.current?.value;
+    const id = Math.floor(Math.random() * Math.random() * 999999999);
+    addTodo(input, id);
+    setValue("");
+  };
   return (
     <div className="Input">
       <Arrow />
-      <input placeholder="what needs to be done?" />
+      <input
+        ref={inputRef}
+        placeholder="what needs to be done?"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            handleSave();
+          }
+        }}
+      />
     </div>
   );
 };
--- src/components/wrapperComponent/index.tsx
+++ src/components/wrapperComponent/index.tsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Input from "../input";
 import TodoList from "../todoList";
 import FooterWrapper from "../footer/footerWrapper";
 import "./style.scss";
 
+// export interface Todo {
+//   id: number;
+//   text: string;
+// }
+
+interface Todo {
+  text: string | undefined;
+  id: number | undefined;
+}
 const WrapperComponent = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodo = (text?: string, id?: number) => {
+    const newTodo: Todo = { text, id };
+    setTodos([...todos, newTodo]);
+  };
+
   return (
     <div className="wrapper">
       <h1>todos</h1>
       <div className="main">
-        <Input />
-        <TodoList />
+        <Input addTodo={addTodo} />
+        <TodoList todos={todos} />
       </div>
       <FooterWrapper />
     </div>
