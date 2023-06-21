895961b45e796a68c134cbf9656bfd6292bdc07a
Ch34ry <pipiasaba788@gmail.com>
2023-06-21 23:41:10 +0400

    modify todos

--- src/components/todoList/index.tsx
+++ src/components/todoList/index.tsx
@@ -12,28 +12,41 @@ interface TodoListI {
   todos: Todo[];
   removeTodo: (id?: number, index?: number) => void;
   selectTodo: (id?: number, index?: number) => void;
+  setValue: (value: string | any) => void;
+  setEditButton: (e: boolean) => void;
+  setModifyID: (id: number | undefined) => void;
   todoClass?: string;
 }
 const TodoList: React.FC<TodoListI> = ({
   todos,
   removeTodo,
   selectTodo,
+  setValue,
+  setEditButton,
+  setModifyID,
   todoClass,
 }) => {
+  const handleClick = (id?: number) => {
+    todos.map((todo) => {
+      if (todo.id === id) {
+        setValue(todo.text);
+        setModifyID(todo.id);
+      }
+    });
+  };
+
   return (
     <div className={`todoList ${todoClass}`}>
       {todos.map((item, index) => {
         return (
           <div className="todoItem" key={index}>
-            <div
-              className="leftPart"
-              onClick={() => selectTodo(item.id, index)}
-            >
+            <div className="leftPart">
               <div
                 className={`icon`}
                 style={{
                   opacity: item.selected ? "1" : "",
                 }}
+                onClick={() => selectTodo(item.id, index)}
               >
                 <Check />
               </div>
@@ -42,6 +55,10 @@ const TodoList: React.FC<TodoListI> = ({
                   textDecoration: item.selected ? "line-through" : "none",
                   color: item.selected ? "#8080808a" : "",
                   fontWeight: item.selected ? "500" : "300",
+                }}
+                onClick={(e) => {
+                  setEditButton(true);
+                  handleClick(item.id);
                 }}
               >
                 {item.text}
